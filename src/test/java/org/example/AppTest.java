7a6bf9003183dc11dae968803dc35327a1ba5476
callegarimattia <mattia.callegari@mail.polimi.it>
2023-02-28 15:42:13 +0100

    JUnit5 configured and first sample test created
    Signed-off-by: callegarimattia <mattia.callegari@mail.polimi.it>

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,34 +1,17 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-        extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+class AppTest {
+    @Test
+    @DisplayName("First Test")
+    void theAnswer() {
+        assertEquals(42, 22 + 20, "ciao");
     }
 }
