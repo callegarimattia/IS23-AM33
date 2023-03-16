c21cfaa2ae4c8f7bc2676ef0dc53eb993ef1598f
Theofanis2001 <theofanis.apostolopoulos@mail.polimi.it>
2023-03-16 23:34:32 +0100

    Bozza classi Player e Shelf - theo

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Player {
+        private Shelf myShelf = new Shelf();
+        private int score;
+        private final String userName;
+        private PersonalGoal personalGoal;
+
+        public Player(String userName) {
+            this.userName = userName;
+        }
+        public calculateScore() {
+
+        }
+}
---
+++ src/main/java/model/Shelf.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Shelf {
+    private optional Tiles[][] matrix;
+
+    public boolean isCommonGoalAchieved(Goal goal){
+
+    }
+    public int scorePersonalGoalAchieved(){
+
+    }
+    public int scoreTilesCluster(){
+
+    }
+
+}
