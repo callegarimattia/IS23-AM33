d89a25d285b52a17ec542a8eb77329ddb5c11d99
Marcodebe01 <marcostefano.debernardi@mail.polimi.it>
2023-03-17 17:50:31 +0100

    Game methods and Board methods

--- src/main/java/model/Game.java
+++ src/main/java/model/Game.java
@@ -1,4 +1,48 @@
 package model;
+import model.CommonGoals.CommonGoal;
+import model.CommonGoals.CommonGoal1;
+import model.PersonalGoals.PersonalGoal;
+
+import java.time.LocalTime;
+import java.util.List;
 
 public class Game {
+    private Board MainBoard;
+    private Player currentTurnPlayer;
+    private int gameID;
+    private LocalTime lastMove;
+    private List<Player> players;
+    /*private CommonGoal pickCommonGoal(){
+    }*/
+
+    public Player getCurrentTurnPlayer(){
+        return currentTurnPlayer;
+    }
+
+    public void nextTurn()
+    {
+
+    }
+    public boolean timeElapsed(){
+        return true;
+    }
+
+    public boolean isCommonGoalAchieved(Player player){
+        return true;
+    }
+
+    public int scorePersonalGoalAchieved(Player player){
+        int score=0;
+        return score;
+    }
+
+    public int scoreTileCluster(Player player){
+        int score =0;
+        return score;
+    }
+
+    public boolean PickTileisAcceptable(Tile tile1){
+
+        return true;
+    }
 }
