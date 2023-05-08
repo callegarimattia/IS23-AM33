9656286e178dd01936ad252049bc911dcd574924
IvanCuppari <ivancuppari@gmail.com>
2023-05-08 13:59:42 +0200

    listener update

--- src/main/java/client/AppClient.java
+++ src/main/java/client/AppClient.java
@@ -1,9 +1,12 @@
 package client;
 
+import server.listenerStuff.ListenerModel;
+
 import java.rmi.RemoteException;
 
 public class AppClient {
     public static void main(String[] args) throws Exception {  // T for TCP, R for RMI
+        ListenerModel myListener = new ListenerModel();
         Client client = null;
         if(args[0]=="T"){
             client = new ClientTCP(args[1]);
@@ -12,7 +15,7 @@ public class AppClient {
 
         else  {
             client = new ClientRMI_Impl(args[1]);
-            ((ClientRMI_Impl) client).joinServer("localhost");   // in futuro non sarà local host
+            ((ClientRMI_Impl) client).joinServer("localhost", myListener);   // in futuro non sarà local host
         }
 
 
--- src/main/java/server/rmi/ServerRMI.java
+++ src/main/java/server/rmi/ServerRMI.java
@@ -1,14 +1,18 @@
 package server.rmi;
 
 import client.ClientRMI;
+import server.listenerStuff.ListenerModel;
 import server.model.lobbies.LobbiesHandlerException;
 import server.model.lobbies.Lobby;
 import server.model.lobbies.User;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface ServerRMI extends Remote {
+
     void createUser(String newUsername) throws LobbiesHandlerException, RemoteException;
 
     void createLobby(User firstUser, int gameSize) throws LobbiesHandlerException, RemoteException;
@@ -16,9 +20,7 @@ public interface ServerRMI extends Remote {
     void joinLobby(User joiningUser, Lobby toBeJoinedlobby) throws LobbiesHandlerException, RemoteException;
 
     void leaveLobby(User leavingUser) throws LobbiesHandlerException, RemoteException;
+    public void setListener(ListenerModel myListener);
 
-    public void join(ClientRMI client) throws RemoteException;  //  new RMI connection
-
-    public  void leave(ClientRMI client) throws RemoteException; // closing RMI connection
-
+    public void removeListener(ListenerModel myListener);
 }
