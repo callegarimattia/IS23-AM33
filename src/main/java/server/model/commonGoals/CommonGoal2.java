e95a80c30e43744e41352d44b23b5d1aac8e83f6
Marcodebe01 <marcostefano.debernardi@mail.polimi.it>
2023-05-16 16:34:58 +0200

    Some Common Goals Implementations

--- src/main/java/server/model/commonGoals/CommonGoal2.java
+++ src/main/java/server/model/commonGoals/CommonGoal2.java
@@ -4,6 +4,18 @@ import server.model.Tile;
 
 public class CommonGoal2 extends CommonGoal{
     public boolean isSolved(Tile[][] matrix) {
+        if(matrix[0][0]==matrix[1][1]&&matrix[0][0]==matrix[2][2]&&matrix[0][0]==matrix[3][3]&&matrix[0][0]==matrix[4][4]){
+            return true;
+        }
+        if(matrix[0][1]==matrix[1][2]&&matrix[0][1]==matrix[2][3]&&matrix[0][1]==matrix[3][4]&&matrix[4][5]==matrix[0][1]){
+            return true;
+        }
+        if(matrix[4][0]==matrix[3][1]&&matrix[4][0]==matrix[2][2]&&matrix[1][3]==matrix[4][0]&&matrix[0][4]==matrix[4][0]){
+            return true;
+        }
+        if(matrix[4][1]==matrix[3][2]&&matrix[4][1]==matrix[2][3]&&matrix[4][1]==matrix[1][4]&&matrix[4][1]==matrix[0][5]){
+            return true;
+        }
         return false;
     }
 
--- src/main/java/server/model/commonGoals/CommonGoal4.java
+++ src/main/java/server/model/commonGoals/CommonGoal4.java
@@ -3,7 +3,42 @@ package server.model.commonGoals;
 import server.model.Tile;
 
 public class CommonGoal4 extends CommonGoal{
+    private final int ROW_NUMBER = 6;
+    private final int COL_NUMBER = 5;
     public boolean isSolved(Tile[][] matrix) {
-        return false;
+        Tile type1;
+        Tile type2 = Tile.EMPTY;
+        Tile type3 = Tile.EMPTY;
+        int counter;
+        int rowCount=0;
+
+        for(int i=0; i<ROW_NUMBER;i++){
+            type1 = matrix[0][i];
+            counter = 1;
+            for(int j=0; j<COL_NUMBER;j++){
+                if(matrix[j][i]!=type1){
+                    if(type2 == Tile.EMPTY){
+                        type2 = matrix[j][i];
+                        counter++;
+                    } else if (type2 != Tile.EMPTY) {
+                        if(matrix[j][i]!=type2){
+                            if(type3 == Tile.EMPTY) {
+                                type3 = matrix[j][i];
+                                counter++;
+                            } else if (type3 != Tile.EMPTY) {
+                                if(type3 != matrix[j][i]){
+                                    counter = ROW_NUMBER;
+                                }
+                            }
+                        }
+                    }
+                    if(counter <= 3){
+                        rowCount++;
+                    }
+
+                }
+            }
+        }
+        return rowCount == 4;
     }
 }
