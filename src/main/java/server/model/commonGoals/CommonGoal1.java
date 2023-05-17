6ca2e078fb93a313e41b91491a7a573a1ba27077
Marcodebe01 <marcostefano.debernardi@mail.polimi.it>
2023-05-17 10:13:54 +0200

    Some common goals implementations

--- src/main/java/server/model/commonGoals/CommonGoal1.java
+++ src/main/java/server/model/commonGoals/CommonGoal1.java
@@ -3,8 +3,15 @@ package server.model.commonGoals;
 import server.model.Tile;
 
 public class CommonGoal1 extends CommonGoal {
+    private final int ROW_NUMBER = 6;
+    private final int COL_NUMBER = 5;
+    int count=0;
     @Override
     public boolean isSolved(Tile[][] matrix) {
-        return false;
+        for (int i=0;i<ROW_NUMBER-1;i++){
+            for(int j=0; j<COL_NUMBER;j++){
+            }
+        }
+    return false;
     }
 }
