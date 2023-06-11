49f7fb47e53a353fbdf2f860b560b321fc291cc9
Marcodebe01 <marcostefano.debernardi@mail.polimi.it>
2023-06-11 10:34:55 +0200

    final common goals correction and testing

--- src/main/java/server/model/commonGoals/CommonGoal12.java
+++ src/main/java/server/model/commonGoals/CommonGoal12.java
@@ -2,67 +2,55 @@ package server.model.commonGoals;
 
 import common.Tile;
 
+
+
 public class CommonGoal12 extends CommonGoal{
     private final int COL_NUMBER = 5;
     public boolean isSolved(Tile[][] matrix) {
         int check = 1;
-        for(int k =0; k< COL_NUMBER; k++){
-            for(int i = 0; i<k; i++){
-                if(matrix[i][k]!=Tile.EMPTY){
-                    k=COL_NUMBER;
-                    check=0;
-                }
-            }
-            if(matrix[k][k]==Tile.EMPTY && k!= COL_NUMBER){
-                k=COL_NUMBER;
-                check=0;
+        int i;
+        for (i = 0; i < COL_NUMBER; i++) {
+            if (matrix[i][i] == Tile.EMPTY) check = 0;
+            System.out.println("SETTO CHECK A 0 : "+check);
+            if (i != 0) {
+                if (matrix[i - 1][i] != Tile.EMPTY) check = 0;
+                System.out.println("SETTO CHECK A 0 : "+check);
             }
         }
-        if(check == 1) return true;
-        check =1;
+        System.out.println("TRUE O FALSE : "+check);
+        if (check == 1) return true;
 
-        for(int k =0; k< COL_NUMBER; k++){
-            for(int i = 0; i<k+1; i++){
-                if(matrix[i][k]!=Tile.EMPTY){
-                    k=COL_NUMBER;
-                    check=0;
-                }
-            }
-            if(matrix[k+1][k]==Tile.EMPTY){
-                k=COL_NUMBER;
-                check=0;
-            }
-        }
-        if(check == 1) return true;
         check =1;
+        System.out.println("FACCIO TORNARE CHECK A 1");
 
-        for(int k=0; k<COL_NUMBER; k++){
-            for(int i=0; i<COL_NUMBER-k-1;i++ ){
-                if(matrix[i][k]!=Tile.EMPTY){
-                    k=COL_NUMBER;
-                    check = 0;
-                }
-                if(matrix[COL_NUMBER-k-1][k]==Tile.EMPTY){
-                    k=COL_NUMBER;
-                    check = 0;
-                }
-            }
+        for (i = 0; i < COL_NUMBER; i++){
+            if(matrix[i+1][i] == Tile.EMPTY || matrix[i][i] != Tile.EMPTY) check = 0;
+            System.out.println("SETTO CHECK A 0 : "+check);
         }
         if(check == 1) return true;
+
         check = 1;
-        for(int k=0; k<COL_NUMBER; k++){
-            for(int i=0; i<COL_NUMBER-k; i++){
-                if(matrix[COL_NUMBER-i][k]!=Tile.EMPTY){
-                    k=COL_NUMBER;
-                    check = 0;
-                }
-                if(matrix[COL_NUMBER-k][k]!=Tile.EMPTY){
-                    k=COL_NUMBER;
-                    check = 0;
-                }
+        System.out.println("FACCIO TORNARE CHECK A 1");
+
+        for(i = 0; i< COL_NUMBER; i++){
+            if(matrix[COL_NUMBER-1-i][i] == Tile.EMPTY)check = 0;
+            System.out.println("SETTO CHECK A 0 : "+check);
+            if(i!= COL_NUMBER-1){
+                if(matrix[COL_NUMBER-2-i][i]!=Tile.EMPTY) check =0;
+                System.out.println("SETTO CHECK A 0 : "+check);
             }
         }
         if(check == 1) return true;
-        return false;
+
+        check =1;
+        System.out.println("FACCIO TORNARE CHECK A 1");
+        for(i = 0; i<COL_NUMBER; i++){
+            if(matrix[COL_NUMBER-i][i] == Tile.EMPTY || matrix[COL_NUMBER-i-1][i]!=Tile.EMPTY) check = 0;
+            System.out.println("SETTO IL CHECK A 0 : "+check);
+        }
+        return check == 1;
     }
+
+
+
 }
---
+++ src/test/java/server/model/commonGoals/CommonGoal12Test.java
@@ -0,0 +1,82 @@
+package server.model.commonGoals;
+import org.junit.jupiter.api.Test;
+import common.Tile;
+import static org.junit.jupiter.api.Assertions.*;
+public class CommonGoal12Test {
+    CommonGoal12 commonGoal12 = new CommonGoal12();
+    Tile[][] emptyMatrix = new Tile[][] {{Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+            {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+            {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+            {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+            {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+            {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+    };
+
+    Tile[][] firstDiagonalMatrix = new Tile[][]
+            {{Tile.PLANT, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.PLANT, Tile.FRAME,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.TROPHY, Tile.CAT,Tile.TROPHY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.FRAME, Tile.CAT,Tile.TROPHY,Tile.PLANT,Tile.EMPTY},
+                    {Tile.TROPHY, Tile.TROPHY,Tile.PLANT,Tile.BOOK,Tile.BOOK},
+                    {Tile.TROPHY, Tile.FRAME,Tile.BOOK,Tile.CAT,Tile.GAME},
+            };
+    Tile[][] secondDiagonalMatrix = new Tile[][]
+            {{Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.PLANT, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.TROPHY, Tile.CAT,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.FRAME, Tile.CAT,Tile.TROPHY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.TROPHY, Tile.TROPHY,Tile.PLANT,Tile.BOOK,Tile.EMPTY},
+                    {Tile.TROPHY, Tile.FRAME,Tile.BOOK,Tile.CAT,Tile.GAME},
+            };
+    Tile[][] thirdDiagonalMatrix = new Tile[][]
+            {{Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.BOOK},
+                    {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.FRAME,Tile.TROPHY},
+                    {Tile.EMPTY, Tile.EMPTY,Tile.TROPHY,Tile.TROPHY,Tile.TROPHY},
+                    {Tile.EMPTY, Tile.CAT,Tile.TROPHY,Tile.PLANT,Tile.BOOK},
+                    {Tile.TROPHY, Tile.TROPHY,Tile.PLANT,Tile.BOOK,Tile.BOOK},
+                    {Tile.TROPHY, Tile.FRAME,Tile.BOOK,Tile.CAT,Tile.CAT},
+            };
+    Tile[][] fourthDiagonalMatrix = new Tile[][]
+            {{Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.EMPTY},
+                    {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.EMPTY,Tile.TROPHY},
+                    {Tile.EMPTY, Tile.EMPTY,Tile.EMPTY,Tile.TROPHY,Tile.TROPHY},
+                    {Tile.EMPTY, Tile.EMPTY,Tile.TROPHY,Tile.PLANT,Tile.BOOK},
+                    {Tile.EMPTY, Tile.TROPHY,Tile.PLANT,Tile.BOOK,Tile.BOOK},
+                    {Tile.TROPHY, Tile.FRAME,Tile.BOOK,Tile.CAT,Tile.CAT},
+            };
+    Tile[][] wrongMatrix = new Tile[][]
+            {{Tile.PLANT, Tile.GAME,Tile.BOOK,Tile.GAME,Tile.BOOK},
+                    {Tile.PLANT, Tile.FRAME,Tile.PLANT,Tile.FRAME,Tile.TROPHY},
+                    {Tile.CAT, Tile.CAT,Tile.TROPHY,Tile.TROPHY,Tile.TROPHY},
+                    {Tile.FRAME, Tile.CAT,Tile.TROPHY,Tile.PLANT,Tile.BOOK},
+                    {Tile.TROPHY, Tile.TROPHY,Tile.PLANT,Tile.BOOK,Tile.BOOK},
+                    {Tile.TROPHY, Tile.FRAME,Tile.BOOK,Tile.CAT,Tile.CAT},
+            };
+    static final boolean wrong = false;
+    static final boolean correct = true;
+    boolean actual;
+
+    @Test
+    void emptyMatrixTest(){
+        assertFalse(commonGoal12.isSolved(emptyMatrix));
+    }
+    @Test
+    void correctMatrixesTest(){
+        assertEquals(correct,commonGoal12.isSolved(firstDiagonalMatrix));
+    }
+    @Test
+    void secondDiagonalMatrixTest(){assertEquals(correct,commonGoal12.isSolved(secondDiagonalMatrix));}
+    @Test
+    void thirdDiagonalMatrixesTest(){
+        assertEquals(correct,commonGoal12.isSolved(thirdDiagonalMatrix));
+    }
+    @Test
+    void fourthDiagonalMatrixesTest(){
+        assertEquals(correct,commonGoal12.isSolved(fourthDiagonalMatrix));
+    }
+
+    @Test
+    void wrongMatrixTest(){
+        assertEquals(wrong,commonGoal12.isSolved(wrongMatrix));
+
+    }}
