54aefb3e568ff5d1e45b5825d8060d1a9a956b78
Mattia Callegari <mattia.callegari@mail.polimi.it>
2023-06-30 11:13:13 +0200

    JavaDoc

---
+++ deliverables/JavaDoc/package-search-index.js
@@ -0,0 +1,5 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "u": "allpackages-index.html"
+}, {"l": "common"}, {"l": "server"}, {"l": "server.controller"}, {"l": "server.exceptions"}, {"l": "server.listenerStuff"}, {"l": "server.model"}, {"l": "server.model.commonGoals"}, {"l": "server.model.personalGoals"}];
+updateSearchResults();
